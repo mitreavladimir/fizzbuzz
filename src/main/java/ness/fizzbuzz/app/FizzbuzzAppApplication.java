71412c6a244e014599a3ffef6d6e80cdcaa5fb2c
Mitrea Vladimir <mitreavladimir@gmail.com>
2020-04-05 13:43:37 +0300

    Initial Project Add

---
+++ src/main/java/ness/fizzbuzz/app/FizzbuzzAppApplication.java
@@ -0,0 +1,13 @@
+package ness.fizzbuzz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FizzbuzzAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ness/fizzbuzz/app/FizzbuzzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ness.fizzbuzz.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
